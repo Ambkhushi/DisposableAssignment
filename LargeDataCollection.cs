0341a19523ef36a28b94e5df2a5289db3169ee42
gsmca128@sgsitsindore.in <gsmca128@sgsitsindore.in>
2023-07-21 12:04:01 +0530

    Add project files.

---
+++ LargeDataCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposableAssignment
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<string> Arraylist = new List<string>();
+        int ArrayCount;
+        bool disposedValue = false;
+        public LargeDataCollection()
+        {
+            Console.WriteLine("Enter the Name:");
+            ArrayCount = int.Parse(Console.ReadLine());
+            for(int i = 0; i < ArrayCount; i++)
+            {
+                Arraylist.Add(Console.ReadLine());
+            }
+        }
+        public void Add(String Name)
+        {
+            Arraylist.Add(Name);
+            ArrayCount++;
+        }
+        public void Remove(String Name)
+        {
+            int index = Arraylist.IndexOf(Name);
+            if (index != -1) { Arraylist.Remove(Name); }
+            else { Console.WriteLine("number is not in list"); }
+        }
+        public void Display()
+        {
+            Console.WriteLine("elements in array as follow:");
+            foreach(var item in Arraylist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    ArrayCount = 0;
+                }
+                Arraylist = null;
+                disposedValue = true;
+                Console.WriteLine($"the Array list has been Disposed");
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposableAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            LargeDataCollection largeDataCollection = new LargeDataCollection();
+
+            do
+            {
+                Console.WriteLine("Enter the operations:");
+                Console.WriteLine("1.Add\n2. Remove\n 3. show");
+                int opt = int.Parse(Console.ReadLine());
+                switch (opt)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("enter the number you want to add:");
+                            string Name = Console.ReadLine();
+                            largeDataCollection.Add(Name);
+                            break;
+                        }
+                    case 2:
+                        {
+
+                            Console.WriteLine("enter the number you want to remove:");
+                            string Name = Console.ReadLine();
+                            largeDataCollection.Remove(Name);
+                            break;
+                        }
+                    case 4:
+                        {
+                            largeDataCollection.Display();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid Input");
+                            break;
+                        }
+                }
+                Console.WriteLine("choose y if u want to continue");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+            Console.WriteLine("All Data has been Disposed");
+            largeDataCollection.Dispose();
+            Console.ReadKey();
+           
+        }
+    }
+}
